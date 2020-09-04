94744d2a7c5871280d31d4dab3e1dffd737818f2
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-04 21:03:35 +0200

    repl

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,4 +1,20 @@
 import instaDirectMessage from "./../../app/src/instaDirectMessage"
-//const testElem = document.querySelector("#test")
+import { sender, recipient } from "./login"
 
-instaDirectMessage()
+instaDirectMessage(sender, recipient).then((sendRaw) => {
+  function send(msg: string, inMin: number = 0) {
+    setTimeout(() => {
+      console.log("sending", msg)
+      sendRaw(msg)
+    }, 1000 * 60 * inMin)
+  }
+
+  send("Nimm mich an")
+  send("Ich bin ein lieber bot", .1)
+  send("Sehr lieb", .2)
+  send("Wenig spam", .3)
+  send(":D", .4)
+
+})
+
+
