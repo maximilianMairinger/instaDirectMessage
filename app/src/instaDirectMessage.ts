ce6cd4bf59eab83c8b8d8bd05647c75f000f2c3c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-04 21:03:26 +0200

    working script

--- app/src/instaDirectMessage.ts
+++ app/src/instaDirectMessage.ts
@@ -1,5 +1,33 @@
+import { IgApiClient, AccountRepositoryLoginResponseLogged_in_user } from "instagram-private-api";
 
-
-export default function() {
+export default async function(sender: {username: string, password: string}, recipient: {username: string}) {
+  let ig = new IgApiClient()
+  console.log("init")
+  ig.state.generateDevice(sender.username)
+  console.log("generateDevice")
+  await ig.simulate.preLoginFlow();
+  console.log("preLoginFlow")
+  let loggedInUser: AccountRepositoryLoginResponseLogged_in_user
+  try {
+    loggedInUser = await ig.account.login(sender.username, sender.password)
+  }
+  catch(e) {
+    throw new Error("Unable to log in via user \"" + sender.username + "\"")
+  }
   
+  let userId: number
+  try {
+    userId = await ig.user.getIdByUsername(recipient.username);
+  }
+  catch(e) {
+    throw new Error("Unable to find recipient with username \"" + recipient.username + "\"")
+  }
+  
+  console.log("myuserid", userId)
+  const thread = ig.entity.directThread([userId.toString()]);
+  console.log("thread")
+
+  return async (msg: string) => {
+    await thread.broadcastText(msg);
+  }
 }
